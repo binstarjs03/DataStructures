2964fdefebf3b064a0f3cd2800aceba34bc73275
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-10 19:38:15 +0700

    Implement List in C#

---
+++ src/CS/IList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace binstarjs03.DataStructures;
+public interface IList<T>
+{
+    /// <summary>
+    /// Gets or sets the element at specified index
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to get or set </param>
+    /// <exception cref="ArgumentOutOfRangeException">when index is less than zero 
+    /// or equal to or larger than <see cref="Count"/></exception>
+    T this[int index] { get; set; }
+
+    /// <summary>
+    /// Gets the number of elements contained in this <see cref="IList{T}"/> 
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Gets or the total number of elements this <see cref="IList{T}"/> 
+    /// can hold without resizing
+    /// </summary>
+    int Capacity { get; }
+
+    /// <summary>
+    /// Adds an instance of <typeparamref name="T"/> to this <see cref="IList{T}"/>
+    /// </summary>
+    /// <param name="item">
+    /// The item to add to this <see cref="IList{T}"/>
+    /// </param>
+    /// <exception cref="ArgumentNullException"></exception>
+    void Add(T item);
+
+    /// <summary>
+    /// Inserts an element into this <see cref="IList{T}"/> at the specified index,
+    /// shifting all the right-side elements of <paramref name="index"/> in
+    /// this <see cref="IList{T}"/> (if any)
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to insert</param>
+    /// <param name="item">
+    /// The object to insert to this <see cref="IList{T}"/>.
+    /// Unlike <see cref="System.Collections.Generic.IList{T}"/>, the value 
+    /// cannot be null for reference types.
+    /// </param>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    void Insert(int index, T item);
+
+    /// <summary>
+    /// Removes the first occurence of a specified object from this <see cref="IList{T}"/>
+    /// </summary>
+    /// <param name="item">
+    /// The object to removed from this <see cref="IList{T}"/>.
+    /// Unlike <see cref="System.Collections.Generic.IList{T}"/>, the value 
+    /// cannot be null for reference types.
+    /// </param>
+    /// <returns>
+    /// <see cref="true"/> if <paramref name="item"/> is successfully found and removed, 
+    /// <see cref="false"/> if <paramref name="item"/> is not found
+    /// </returns>
+    bool Remove(T item);
+
+    /// <summary>
+    /// Removes the element at the specified index of this <see cref="IList{T}"/>
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to remove</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    void RemoveAt(int index);
+
+    /// <summary>
+    /// Removes the last element in this <see cref="IList{T}"/> and return it
+    /// </summary>
+    /// <returns>The removed object</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    T Pop();
+
+    /// <summary>
+    /// Removes all elements in this <see cref="IList{T}"/>
+    /// </summary>
+    void Clear();
+
+    /// <summary>
+    /// Sets the capacity to the actual number of elements in this <see cref="IList{T}"/>
+    /// </summary>
+    void TrimExcess();
+}
---
+++ src/CS/List.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace binstarjs03.DataStructures;
+public class List<T> : IList<T>
+{
+    private T[] _array;
+    private int _count = 0;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="List{T}"/> 
+    /// class that is empty and has the default initial  capacity.
+    /// </summary>
+    public List()
+    {
+        _array = new T[4];
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="List{T}"/> 
+    /// class that is empty and has the specified initial capacity.
+    /// </summary>
+    public List(int capacity)
+    {
+        _array = new T[capacity];
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= Count || index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            return _array[index];
+        }
+        set
+        {
+            if (index >= Count || index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            _array[index] = value;
+        }
+    }
+
+    public int Count
+    {
+        get => _count;
+        private set => _count = value;
+    }
+    
+    public int Capacity => _array.Length;
+
+    public void Add(T item)
+    {
+        if (item is null)
+            throw new ArgumentNullException(nameof(item));
+        if (Capacity == Count)
+            Resize(Capacity * 2, Count);
+        _array[Count++] = item;
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < Count; i++)
+            _array[i] = default!;
+        Count = 0;
+    }
+    
+    public void Insert(int index, T item)
+    {
+        if (item is null)
+            throw new ArgumentNullException(nameof(item));
+        // we can insert at exactly end of the list (after last item),
+        // but not overflowing it, so comparison is not "equal to or larger"
+        if (index > Count || index < 0)
+            throw new ArgumentOutOfRangeException(nameof(index));
+        if (Capacity == Count)
+            Resize(Capacity * 2, Count);
+        ShiftInsertItem(index);
+        _array[index] = item;
+        Count++;
+    }
+    
+    public T Pop()
+    {
+        if (Count < 0)
+            throw new InvalidOperationException();
+        int index = Count - 1;
+        T result = _array[index];
+        _array[index] = default!;
+        Count--;
+        return result;
+    }
+    
+    public bool Remove(T item)
+    {
+        if (item is null)
+            throw new ArgumentNullException(nameof(item));
+        bool removed = false;
+        for (int index = 0; index < Count; index++)
+        {
+            if (!_array[index]!.Equals(item))
+                continue;
+            _array[index] = default!;
+            removed = true;
+            ShiftEmptyItem(index);
+            Count--;
+            break;
+        }
+        return removed;
+    }
+    
+    public void RemoveAt(int index)
+    {
+        if (index >= Count || index < 0)
+            throw new ArgumentOutOfRangeException(nameof(index));
+        _array[index] = default!;
+        ShiftEmptyItem(index);
+        Count--;
+    }
+
+    public void TrimExcess()
+    {
+        Resize(Count, Count);
+    }
+
+    private void Resize(int newCapacity, int oldItemCount)
+    {
+        T[] newArray = new T[newCapacity];
+        _array = Relocate(newArray, _array, oldItemCount);
+    }
+
+    private static T[] Relocate(T[] newArray, T[] oldArray, int itemCount)
+    {
+        for (int i = 0; i < itemCount; i++)
+            newArray[i] = oldArray[i];
+        return newArray;
+    }
+
+    private void ShiftEmptyItem(int emptyIndex)
+    {
+        // shift deleted item from left to right, this is to fill the empty space
+        int tailPos = Count - 1;
+        for (int index = emptyIndex; index < tailPos; index++)
+            (_array[index], _array[index + 1]) = (_array[index + 1], _array[index]);
+    }
+
+    private void ShiftInsertItem(int insertionIndex)
+    {
+        // shift empty space from right to left, this is to make a room for insertion
+        int emptyIndex = Count;
+        for (int index = emptyIndex; index > insertionIndex; index--)
+            (_array[index], _array[index - 1]) = (_array[index - 1], _array[index]);
+    }
+}
